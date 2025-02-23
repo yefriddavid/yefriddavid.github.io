913bfc376928a80888b0efbf75123a6bb17a22f6
dave <yefriddavid.apps@gmail.com>
2025-02-23 07:17:11 -0500

    login improve

--- src/App.js
+++ src/App.js
@@ -19,6 +19,12 @@ const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
 const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
 const AboutMe = React.lazy(() => import('./views/pages/aboutMe/Index'))
 
+
+// temporary line
+localStorage.setItem('coreui-free-react-admin-template-theme', 'light')
+
+
+
 const App = () => {
   const { isColorModeSet, setColorMode } = useColorModes('coreui-free-react-admin-template-theme')
   const storedTheme = useSelector((state) => state.theme)
