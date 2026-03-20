9f43bafe02196943bb2c1b08b05b56ca5f3f3ed0
dave <yefriddavid.apps@gmail.com>
2026-03-20 18:10:35 -0500

    feat: translate breadcrumb using tKey on routes

--- src/components/AppBreadcrumb.js
+++ src/components/AppBreadcrumb.js
@@ -1,29 +1,27 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
 
 import routes from '../routes'
 
 import { CBreadcrumb, CBreadcrumbItem } from '@coreui/react'
 
 const AppBreadcrumb = () => {
+  const { t } = useTranslation()
   const currentLocation = useLocation().pathname
-
-  const getRouteName = (pathname, routes) => {
-    const currentRoute = routes.find((route) => route.path === pathname)
-    return currentRoute ? currentRoute.name : false
-  }
 
   const getBreadcrumbs = (location) => {
     const breadcrumbs = []
     location.split('/').reduce((prev, curr, index, array) => {
       const currentPathname = `${prev}/${curr}`
-      const routeName = getRouteName(currentPathname, routes)
-      routeName &&
+      const currentRoute = routes.find((route) => route.path === currentPathname)
+      if (currentRoute) {
         breadcrumbs.push({
           pathname: currentPathname,
-          name: routeName,
-          active: index + 1 === array.length ? true : false,
+          name: currentRoute.tKey ? t(currentRoute.tKey) : currentRoute.name,
+          active: index + 1 === array.length,
         })
+      }
       return currentPathname
     })
     return breadcrumbs
@@ -33,17 +31,15 @@ const AppBreadcrumb = () => {
 
   return (
     <CBreadcrumb className="my-0">
-      <CBreadcrumbItem href="/">Home</CBreadcrumbItem>
-      {breadcrumbs.map((breadcrumb, index) => {
-        return (
-          <CBreadcrumbItem
-            {...(breadcrumb.active ? { active: true } : { href: breadcrumb.pathname })}
-            key={index}
-          >
-            {breadcrumb.name}
-          </CBreadcrumbItem>
-        )
-      })}
+      <CBreadcrumbItem href="/">{t('nav.home')}</CBreadcrumbItem>
+      {breadcrumbs.map((breadcrumb, index) => (
+        <CBreadcrumbItem
+          {...(breadcrumb.active ? { active: true } : { href: breadcrumb.pathname })}
+          key={index}
+        >
+          {breadcrumb.name}
+        </CBreadcrumbItem>
+      ))}
     </CBreadcrumb>
   )
 }
