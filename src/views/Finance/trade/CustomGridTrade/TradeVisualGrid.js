c2a2d95fea0962aa887ffea009c31523df72c64c
dave <yefriddavid.apps@gmail.com>
2026-05-03 16:50:53 -0500

    phone improves v2

--- src/views/Finance/trade/CustomGridTrade/TradeVisualGrid.js
+++ src/views/Finance/trade/CustomGridTrade/TradeVisualGrid.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useRef } from 'react'
+import React, { useState, useMemo, useRef, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import {
   CModal,
