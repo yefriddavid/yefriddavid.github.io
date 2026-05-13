d22f0772a56a950f040c72ed3eb39e59c95c966f
dave <yefriddavid.apps@gmail.com>
2026-05-13 11:23:57 -0500

    menu finance improve

--- src/_nav.js
+++ src/_nav.js
@@ -132,25 +132,7 @@ const getNav = (t, role) => {
       ],
     })
   }
-  items.push({
-    component: CNavGroup,
-    name: 'Domótica',
-    icon: <CIcon icon={cilHome} customClassName="nav-icon" />,
-    items: [
-      {
-        component: CNavItem,
-        name: 'Panel Solar',
-        to: '/domotica/solar',
-        icon: <CIcon icon={cilLightbulb} customClassName="nav-icon" />,
-      },
-      {
-        component: CNavItem,
-        name: 'Dispositivos',
-        to: '/domotica/devices',
-        icon: <CIcon icon={cilSettings} customClassName="nav-icon" />,
-      },
-    ],
-  })
+
 
   items.push({
     component: CNavItem,
