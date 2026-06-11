69a91cba749a455e32d336e8406f6c1409fcc50f
dave <yefriddavid.apps@gmail.com>
2026-06-11 18:38:35 -0500

    fix(calc-list): narrow index column width

--- src/views/Finance/trade/CalcList/index.js
+++ src/views/Finance/trade/CalcList/index.js
@@ -9,7 +9,7 @@ import SyncModal from './SyncModal'
 import './CalcList.scss'
 
 const COLUMNS = [
-  { key: 'index', label: '#', type: 'readonly', width: 44 },
+  { key: 'index', label: '#', type: 'readonly', width: 32 },
   { key: 'description', label: 'Description', type: 'text' },
   { key: 'category',       label: 'Category',       type: 'select', options: CALC_LIST_CATEGORIES,    width: 110 },
   { key: 'classification', label: 'Classification', type: 'select', options: CALC_LIST_CLASSIFICATIONS, width: 120 },
